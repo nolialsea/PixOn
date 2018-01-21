9e593d9f942ab10f6b7932352742a12802eeb4bf
nolialsea <nolialsea@hotmail.fr>
2018-01-21 02:12:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,40 +9,10 @@ console.log(port+' is the magic port');
 app.set('view engine', 'ejs');
 app.use('/views', express.static(__dirname + '/views'));
 
+
 var db = new sqlite3.Database('db.db');
 
 db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS UserRank("+
-      "id INTEGER PRIMARY KEY,"+
-      "name TEXT UNIQUE"+
-  ")");
-
-  db.run("CREATE TABLE IF NOT EXISTS User("+
-      "id INTEGER PRIMARY KEY, "+
-      "login TEXT UNIQUE,"+
-      "password TEXT,"+
-      "rank INTEGER,"+
-      "dateCreation INTEGER"+
-  ")");
-
-  db.run("CREATE TABLE IF NOT EXISTS TaskState("+
-      "id INTEGER PRIMARY KEY,"+
-      "name TEXT UNIQUE"+
-  ")");
-
-  db.run("CREATE TABLE IF NOT EXISTS TaskFeature("+
-      "id INTEGER PRIMARY KEY,"+
-      "name TEXT UNIQUE"+
-  ")");
-
-  db.run("CREATE TABLE IF NOT EXISTS Task("+
-      "id INTEGER PRIMARY KEY,"+
-      "feature INTEGER,"+
-      "task TEXT,"+
-      "owner INTEGER,"+
-      "state INTEGER,"+
-      "dateCreation INTEGER"+
-  ")");
 
   db.run("CREATE TABLE IF NOT EXISTS Pixel("+
       "id INTEGER PRIMARY KEY,"+
@@ -56,43 +26,6 @@ db.serialize(function() {
       "dateCreation INTEGER"+
   ")");
 
-  db.all("SELECT id FROM TaskState", function(err, rows){
-    if (!err && (!rows || rows.length == 0)){
-      var stmt = db.prepare("INSERT INTO TaskFeature (name) VALUES (?)");
-      stmt.run("General");
-      stmt.run("Pixon");
-      stmt.run("Todo");
-      stmt.run("Account");
-      stmt.run("Scripting");
-      stmt.run("Code");
-      stmt.finalize();
-
-      stmt = db.prepare("INSERT INTO TaskState (name) VALUES (?)");
-      stmt.run("None");
-      stmt.run("Accepted");
-      stmt.run("Refused");
-      stmt.run("In progress");
-      stmt.run("Paused");
-      stmt.run("Finished");
-      stmt.finalize();
-
-      stmt = db.prepare("INSERT INTO UserRank (name) VALUES (?)");
-      stmt.run("Admin");
-      stmt.run("Moderator");
-      stmt.run("Contributor");
-      stmt.run("Banned");
-      stmt.finalize();
-    }
-  })
-
-});
-
-app.get('/todo', function(req, res) {
-  getAllTaskFeatures(function(taskFeatures){
-    getAllTaskStates(function(taskStates){
-      res.render('todo', {taskFeatures: taskFeatures, taskStates: taskStates});
-    });
-  });
 });
 
 app.get('*', function(req, res) {
@@ -105,129 +38,6 @@ var conf = {
     gridHeight: 128,
     nbChannel: 1
 };
-
-//Account
-function insertUser(login, password, callback){
-  getUserByLogin(login, function(user){
-    if(user == null){
-      var stmt = db.prepare("INSERT INTO User (login, password, rank, dateCreation) VALUES (?,?,?,?)");
-      stmt.run(login, md5(password), 0, new Date().getTime());
-      stmt.finalize();
-      callback(true);
-    }else{
-      callback(false);
-    }
-  });
-}
-
-function getUserByLogin(userLogin, callback){
-  db.all("SELECT * FROM User WHERE login='"+userLogin+"'", function(err, rows) {
-    if (!err){
-      if (rows.length == 1){
-        callback(rows[0]);
-      }else{
-        callback(null);
-      }
-    }else{
-      callback(null);
-    }
-  });
-}
-
-function getUserById(userId, callback){
-  db.all("SELECT * FROM User WHERE id="+userId, function(err, rows) {
-    if (!err){
-      if (rows.length == 1){
-        callback(rows[0]);
-      }else{
-        callback(null);
-      }
-    }else{
-      callback(null);
-    }
-  });
-}
-
-function authenticateUser(userLogin, userPassword, callback){
-  getUserByLogin(userLogin, function(user){
-    if (user != null){
-      if (user.password == md5(userPassword)){
-        callback(true);
-      }else{
-        callback(false);
-      }
-    }else{
-      insertUser(userLogin, userPassword, function(result){
-        callback(result);
-      });
-    }
-  });
-}
-
-//Todo list
-function insertTask(task, userId){
-  var stmt = db.prepare("INSERT INTO Task (task, owner, state, feature, dateCreation) VALUES (?,?,?,?,?)");
-  stmt.run(task.task, userId, 1, task.feature, new Date().getTime());
-  stmt.finalize();
-}
-
-function deleteTask(taskId, callback){
-  var stmt = db.prepare("DELETE FROM Task WHERE id ="+taskId);
-  stmt.run();
-  stmt.finalize();
-  callback();
-}
-
-function getTask(taskId, callback){
-  db.serialize(function() {
-    db.each("SELECT * FROM Task WHERE id="+taskId, function(err, row) {
-      if (!err){
-        callback(row);
-      }
-    });
-  });
-}
-
-function getLastTask(callback){
-  db.serialize(function() {
-    db.each("SELECT * FROM Task ORDER BY id DESC LIMIT(1)", function(err, row) {
-      if (!err){
-        getUserById(row.owner, function(user){
-          if (user){
-            row.owner = user.login;
-            callback(row);
-          }else{
-            callback(null);
-          }
-        });
-      }
-    });
-  });
-}
-
-function getAllTasks(callback){
-  db.all("SELECT * FROM Task", function(err, rows) {
-    if (!err){
-      callback(rows);
-    }
-  });
-}
-
-function getAllTaskFeatures(callback){
-    db.all("SELECT * FROM TaskFeature", function(err, rows) {
-        if (!err){
-            callback(rows);
-        }
-    });
-}
-
-function getAllTaskStates(callback){
-    db.all("SELECT * FROM TaskState", function(err, rows) {
-        if (!err){
-            callback(rows);
-        }
-    });
-}
 
 
 //PixOn
@@ -307,53 +117,6 @@ io.on('connection', function (socket) {
         socket.emit('pixels', rows);
     });
 
-    socket.on('login', function(user){
-      user.login = user.login.trim();
-      authenticateUser(user.login, user.password, function(success){
-        if (success){
-          getUserByLogin(user.login, function(user_){
-            socket.data.login = user.login;
-            socket.data.userId = user_.id;
-            socket.emit('login', user_.id);
-            getAllTasks(function(rows){
-                socket.emit('allTasks', rows);
-            });
-          });
-        }else{
-          socket.emit('login', null);
-        }
-
-      });
-    });
-
-    socket.on('getAllTasks', function(){
-      getAllTasks(function(rows){
-          socket.emit('allTasks', rows);
-      });
-    });
-
-    socket.on('changeTaskState', function(data){
-      if (socket.data.userId != 0){
-        db.serialize(function(){
-          db.run("UPDATE Task SET state='"+data.taskStateId+"' WHERE id="+data.taskId+"");
-          getAllTasks(function(rows){
-              io.emit('allTasks', rows);
-          });
-        });
-      }
-    });
-
-    socket.on('changeTaskFeature', function(data){
-      if (socket.data.userId != 0){
-        db.serialize(function(){
-          db.run("UPDATE Task SET feature='"+data.taskFeatureId+"' WHERE id="+data.taskId+"");
-          getAllTasks(function(rows){
-              io.emit('allTasks', rows);
-          });
-        });
-      }
-    });
-
     socket.on('getAllPixelsAt', function (timestamp) {
         getAllPixelsAt(timestamp, function(rows){
             socket.emit('allPixelsAt', rows);
@@ -379,36 +142,7 @@ io.on('connection', function (socket) {
         }
     });
 
-    socket.on('task', function(task){
-      if (socket.data.userId != 0){
-        insertTask(task, socket.data.userId);
-        getAllTasks(function(rows){
-          io.emit('allTasks', rows);
-        });
-      }
-    });
-
-    socket.on('deleteTask', function(taskId){
-      if (socket.data.userId != 0){
-        getTask(taskId, function(task){
-          if (task.owner == socket.data.userId){
-            deleteTask(taskId, function(){
-              getAllTasks(function(rows){
-                  io.emit('allTasks', rows);
-              });
-            });
-          }
-        })
-
-      }
-    });
-
-    socket.on('getAllTasks', function () {
-      getAllTasks(function(tasks){
-        socket.emit('allTasks', tasks);
-      });
-    });
-
+    
     socket.on('disconnect', function () {
 
     });
